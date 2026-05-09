fa617e814174bf5b2f156d9eaf6d63e2310e7d8e
Charlie Howard <charlie@howardgb.com>
2026-05-09 11:08:52 +0100

    Updated Nextcloud Script

--- NextCloud/nextcloudSync.sh
+++ NextCloud/nextcloudSync.sh
@@ -1,7 +1,6 @@
 #!/bin/bash
 set -euo pipefail
 
-WATCH_DIR="/path/to/Nextcloud"
-REMOTE="https://username:password@nextclouddomain.com"
+WATCH_DIR="/mnt/porygonStorage/Nextcloud"
 
 exec /usr/bin/nextcloudcmd "$WATCH_DIR" "$REMOTE"
